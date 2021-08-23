81ab9f3f24402306d29d0c440bdf38b3bdbd6cf4
yury trypolski <yury1011.89@gmail.com>
2021-08-23 14:02:41 +0300

    added components pages and navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="container pt-4">
         <Switch>
-          <Route />
+          <Route exact path={"/"} component={Home} />
+          <Route path={"/about"} component={About} />
         </Switch>
       </div>
     </BrowserRouter>
